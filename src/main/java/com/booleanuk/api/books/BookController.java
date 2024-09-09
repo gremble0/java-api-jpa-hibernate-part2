10cc7159a34bed3357e465c58f36ecd4f9f03488
gremble0 <hermanstornes@gmail.com>
2024-09-09 15:19:50 +0200

    Finish core

--- src/main/java/com/booleanuk/api/books/BookController.java
+++ src/main/java/com/booleanuk/api/books/BookController.java
@@ -1,13 +1,16 @@
 package com.booleanuk.api.books;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +45,31 @@ public class BookController {
         .map(ResponseEntity::ok)
         .orElseThrow(
             () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No book with id '" + id + "' exists"));
+  }
+
+  @PutMapping(value = "{id}")
+  public ResponseEntity<Book> put(@PathVariable int id, @RequestBody Book book) throws ResponseStatusException {
+    Optional<Book> maybeBookToUpdate = this.repository.findById(id);
+    if (maybeBookToUpdate.isEmpty())
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No book with id '" + id + "' exists");
+
+    Book bookToUpdate = maybeBookToUpdate.get();
+    bookToUpdate.setTitle(book.getTitle());
+    bookToUpdate.setGenre(book.getGenre());
+    bookToUpdate.setAuthorId(book.getAuthorId());
+    bookToUpdate.setPublisherId(book.getPublisherId());
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(this.repository.save(bookToUpdate));
+  }
+
+  @DeleteMapping(value = "{id}")
+  public ResponseEntity<Book> delete(@PathVariable int id) throws ResponseStatusException {
+    Optional<Book> maybeBookToDelete = this.repository.findById(id);
+    if (maybeBookToDelete.isEmpty())
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No book with id '" + id + "' exists");
+
+    this.repository.deleteById(id);
+    return ResponseEntity.ok(maybeBookToDelete.get());
   }
 }
